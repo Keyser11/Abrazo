96c68196bb6138783d0535e90c8fefa289768e16
Mariobrosh44 <128196730+Mariobrosh44@users.noreply.github.com>
2023-03-23 00:39:04 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,23 @@
+function CambiaMsj() {
+    document.getElementById("loading-msj").innerText = "Enviado con Exito!!"
+}
+
+setTimeout(CambiaMsj,6000)
+
+setTimeout(function() {
+    alert(" Keyser Te Ha Enviado Un Abrazo❤️")
+}, 7200)
+
+setTimeout(function() {
+    alert("Un Abrazo No Es Suficiente🙈😊")
+}, 7200)
+
+setTimeout(function() {
+    alert("🕗...")
+}, 7200)
+
+setTimeout(function() {
+    alert("Recibiste Un Segundo Abrazo 🙈😚❤️")
+}, 7200)
+
+
