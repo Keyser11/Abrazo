62355762f86a563d26ef259917285011f7c33e8b
Mariobrosh44 <128196730+Mariobrosh44@users.noreply.github.com>
2023-04-01 22:05:17 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,5 +1,5 @@
 function CambiaMsj() {
-    document.getElementById("loading-msj").innerText = "Enviado con Exito ✓!!"
+    document.getElementById("loading-msj").innerText = "Enviado con Exito ✅!!"
 }
 
 setTimeout(CambiaMsj,6000)
